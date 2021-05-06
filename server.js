d25e8f515c73cd7d4f2e132d01263bd15ba4f6cc
Ricardo Almeida da Rocha <ricardo.rocha@dellead.com>
2021-05-06 16:46:53 -0300

    test path

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-const app = express();
-app.use(requireHTTPS);
-app.get("/", function(req, res) {
-    res.sendFile("index.html", {root: "src/index.html"}
-  );
-  });
+
+// Serve only the static files form the dist directory
+app.use(express.static('./dist/crud-front'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/crud-front/'}),
+);
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
